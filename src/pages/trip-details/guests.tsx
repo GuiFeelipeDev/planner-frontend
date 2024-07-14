23a95243daaae61d006733e89a7da5b210a4e542
Guilherme Pereira <guifelipedev@gmail.com>
2024-07-13 22:01:29 -0400

    feat: added delete button in the links and guests

--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,18 +1,26 @@
-import { CheckCircle, CircleDashed, UserCog } from "lucide-react"
+import { CheckCircle, CircleDashed, Trash2, UserCog } from "lucide-react"
 import { Button } from "../../components/button"
 import { useParams } from "react-router-dom"
 import { api } from "../../lib/axios"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { useState } from "react"
 
 interface Participant {
   id: string
   name: string | null
   email: string
   is_confirmed: boolean
+  is_owner?: boolean
 }
 
-const Guests = () => {
+interface GuestsProps {
+  openAddParticipantModal: () => void
+}
+
+const Guests = ({ openAddParticipantModal }: GuestsProps) => {
   const { tripId } = useParams()
+
+  const [guestInCurrentHover, setGuestInCurrentHover] = useState("")
 
   const { data: guests } = useQuery<Participant[]>({
     queryKey: ["fetchGuests", tripId],
@@ -21,6 +29,29 @@ const Guests = () => {
         .get(`/trips/${tripId}/participants`)
         .then((res) => res.data.participants),
   })
+
+  const showDeleteButton = (index: string) => {
+    setGuestInCurrentHover(index)
+  }
+
+  const hideDeleteButton = () => {
+    setGuestInCurrentHover("")
+  }
+
+  const queryClient = useQueryClient()
+
+  const deleteGuest = useMutation({
+    mutationFn: (guestId: string) => api.delete("/participants/" + guestId),
+    mutationKey: ["deleteGuest"],
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["fetchGuests"] }),
+  })
+
+  const handleDeleteGuest = (id: string) => {
+    if (!id) return
+
+    deleteGuest.mutate(id)
+  }
 
   return (
     <div className="space-y-6">
@@ -31,16 +62,28 @@ const Guests = () => {
             <div
               key={guest.id}
               className="flex justify-between items-center gap-4"
+              onMouseEnter={() => showDeleteButton(guest.id || "")}
+              onMouseLeave={hideDeleteButton}
             >
               <div className="space-y-1.5">
                 <span className="block font-medium text-zinc-100">
-                  {guest.name || "Não confirmado"}
+                  {guest.name || "Não confirmado"}{" "}
+                  <span className="text-zinc-400 text-xs">
+                    {guest.is_owner && "(criador)"}
+                  </span>
                 </span>
                 <span className="block text-sm text-zinc-400 ">
                   {guest.email}
                 </span>
               </div>
-              {guest.is_confirmed ? (
+              {guest.id === guestInCurrentHover && !guest.is_owner ? (
+                <button
+                  type="button"
+                  onClick={() => handleDeleteGuest(guest.id || "")}
+                >
+                  <Trash2 className="size-5 text-red-500" />
+                </button>
+              ) : guest.is_confirmed ? (
                 <CheckCircle className="size-5 text-lime-300 shrink-0" />
               ) : (
                 <CircleDashed className="size-5 text-zinc-400 shrink-0" />
@@ -50,7 +93,12 @@ const Guests = () => {
         })}
       </div>
 
-      <Button variant="secondary" size="full">
+      <Button
+        onClick={openAddParticipantModal}
+        type="button"
+        variant="secondary"
+        size="full"
+      >
         <UserCog className="size-5 " />
         Gerenciar convidados
       </Button>
--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,9 +1,10 @@
-import { Link2, Plus } from "lucide-react"
+import { Link2, Plus, Trash2 } from "lucide-react"
 import { Button } from "../../components/button"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { useParams } from "react-router-dom"
 import { TripUrl } from "../../interfaces/interfaces"
 import { api } from "../../lib/axios"
+import { useState } from "react"
 
 interface ImportantLinksProps {
   openCreateLinkModal: () => void
@@ -11,12 +12,36 @@ interface ImportantLinksProps {
 
 const ImportantLinks = ({ openCreateLinkModal }: ImportantLinksProps) => {
   const { tripId } = useParams()
+  const [linkInCurrentHover, setLinkInCurrentHover] = useState("")
 
   const { data: tripLinks } = useQuery<TripUrl[]>({
     queryKey: ["fetchLinks", tripId],
     queryFn: () =>
       api.get(`/trips/${tripId}/links`).then((res) => res.data.links),
   })
+
+  const showDeleteButton = (index: string) => {
+    setLinkInCurrentHover(index)
+  }
+
+  const hideDeleteButton = () => {
+    setLinkInCurrentHover("")
+  }
+
+  const queryClient = useQueryClient()
+
+  const deleteLink = useMutation({
+    mutationFn: (linkId: string) => api.delete("/links/" + linkId),
+    mutationKey: ["deleteLink"],
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["fetchLinks"] }),
+  })
+
+  const handleDeleteLink = (id: string) => {
+    if (!id) return
+
+    deleteLink.mutate(id)
+  }
 
   return (
     <div className="space-y-6">
@@ -30,19 +55,30 @@ const ImportantLinks = ({ openCreateLinkModal }: ImportantLinksProps) => {
             <div
               key={link.id}
               className="flex justify-between items-center gap-4"
+              onMouseEnter={() => showDeleteButton(link.id || "")}
+              onMouseLeave={hideDeleteButton}
             >
               <div className="space-y-1.5">
                 <span className="block font-medium text-zinc-100">
                   {link.title}
                 </span>
                 <a
-                  href="#"
+                  href={link.url}
                   className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
                 >
                   {link.url}
                 </a>
               </div>
-              <Link2 className="size-5 text-zinc-400 shrink-0" />
+              {link.id === linkInCurrentHover ? (
+                <button
+                  type="button"
+                  onClick={() => handleDeleteLink(link.id || "")}
+                >
+                  <Trash2 className="size-5 text-red-500" />
+                </button>
+              ) : (
+                <Link2 className="size-5 text-zinc-400 shrink-0" />
+              )}
             </div>
           )
         })}
