5111703e9df1d7f2358d7bf1313e6b7d9a238ca0
Guilherme Pereira <guifelipedev@gmail.com>
2024-07-17 13:43:23 -0400

    feat: added Trip and Local storage interfaces

--- src/interfaces/interfaces.tsx
+++ src/interfaces/interfaces.tsx
@@ -3,3 +3,16 @@ export interface TripUrl {
   title: string
   url: string
 }
+
+export interface Trip {
+  id: string
+  destination: string
+  starts_at: string
+  ends_at: string
+  is_confirmed: boolean
+}
+
+export interface localUserProps {
+  id: string
+  email: string
+}
